9760d7d31230a4d60afc6c517a9548c79efb6d41
Minte021 <mintesnotalemayehu21@gmail.com>
2026-04-09 22:57:18 +0300

    note app has built

--- note-app/script.js
+++ note-app/script.js
@@ -1,3 +1,19 @@
-Function addNote() {
+function addNote() {
+    let text = document.getElementById("note").value;
+    if (text.trim() === "") return;
+    let li = document.createElement("li");
+    li.innerHTML = text;
     
+    let btn = document.createElement("button");
+    btn.innerHTML = "Delete Note";
+
+    document.getElementById("note-list").appendChild(li);
+    document.getElementById("note-list").appendChild(btn);
+
+    btn.onclick = function() {
+        li.remove();
+        btn.remove();
+    }
+    
+    document.getElementById("note").value = "";
 }
