87cfb5a61189395ab9ab2af8face6643d800b526
Minte021 <mintesnotalemayehu21@gmail.com>
2026-04-13 14:50:34 +0300

    edit function added

--- note-app/script.js
+++ note-app/script.js
@@ -1,20 +1,37 @@
 function addNote() {
-    let text = document.getElementById("note").value;
+    let noteInput = document.getElementById("note");
+    let text = noteInput.value;
     if (text.trim() === "") return;
+
+    let delete_btn = document.createElement("button");
+    delete_btn.id = "delete-btn";
+    delete_btn.innerText = "Delete";
+
+    let edit_btn = document.createElement("button");
+    edit_btn.id = "edit-btn";
+    edit_btn.innerText = "Edit";
+
     let li = document.createElement("li");
-    li.innerHTML = text;
-    
-    let btn = document.createElement("button");
-    btn.id = "delete-btn";
-    btn.innerHTML = "Delete";
+    li.innerText = text;
 
-    document.getElementById("note-list").appendChild(li);
-    document.getElementById("note-list").appendChild(btn);
+    let note_lst = document.getElementById("note-list");
+    note_lst.appendChild(li);
+    note_lst.appendChild(delete_btn);
+    note_lst.appendChild(edit_btn);
 
-    btn.onclick = function() {
-        li.remove();
-        btn.remove();
+    delete_btn.onclick = function() {
+        note_lst.removeChild(li);
+        note_lst.removeChild(delete_btn);
+        note_lst.removeChild(edit_btn);
     }
-    
+
+    edit_btn.onclick = function() {
+        noteInput.value = text;
+        note_lst.removeChild(li);
+        note_lst.removeChild(delete_btn);
+        note_lst.removeChild(edit_btn);
+    }
+
+
     document.getElementById("note").value = "";
 }
