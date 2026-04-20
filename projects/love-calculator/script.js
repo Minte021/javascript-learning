25541de84a1b102c58e0cd8dbc4d310bf19ca206
Minte021 <mintesnotalemayehu21@gmail.com>
2026-04-20 18:46:23 +0300

    bug fixed

--- projects/love-calculator/script.js
+++ projects/love-calculator/script.js
@@ -1,8 +1,17 @@
 function calculateLove() {
-    let percent = Math.random();
-    percent *= 100;
-    percent = Math.floor(percent);
-    percent += 1;
+  let percent = Math.random();
+  percent *= 100;
+  percent = Math.floor(percent);
+  percent += 1;
 
-    document.getElementById("result").innerHTML = percent;
+  let name1 = document.getElementById("name1").value;
+  let name2 = document.getElementById("name2").value;
+  if (name1 === name2 && name1 !== "" && name2 !== "") {
+    percent = 100;
+  } else if (name1 === "" || name2 === "") {
+    percent = 0;
+  }
+  document.getElementById("result").innerHTML = percent;
+  document.getElementById("name1").value = "";
+  document.getElementById("name2").value = "";
 }
